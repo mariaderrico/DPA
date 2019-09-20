0d57fa606a8effc8a7b6eb8d52287661d67d136d
Maria d'Errico <derrico@immunology.uzh.ch>
2019-09-20 19:01:01 +0200

    Add the algorithm for the optimal k_hat in the PAk density estimator

--- DPA/PAk.py
+++ DPA/PAk.py
@@ -2,18 +2,88 @@
 The pointwise-adaptive k-NN density estimator.
 """
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.base import BaseEstimator, DensityMixin, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import euclidean_distances
 from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import kneighbors_graph
+from math import log, sqrt, exp, lgamma, pi, pow
 #from . import twoNN
 
 
 VALID_METRIC = ['precomputed', 'euclidean']
 VALID_DIM = ['auto', 'two_nn']
 
-class PointAdaptive_kNN(BaseEstimator):
+def _PointAdaptive_kNN(distances, indices, k_max=1000, D_thr=23.92812698, dim=None):
+    """
+    Parameters
+    ----------
+    distances: array [n_samples, k_max]
+        Distances to the k_max neighbors of each points.
+  
+    indices : array [n_samples, k_max]
+        Indices of the k_max neighbors of each points. 
+
+    k_max : int, default=1000
+        The maximum number of nearest-neighbors considered by the procedure that returns the
+        largest number of neighbors ``\hat{k}`` for which the condition of constant density 
+        holds, within a given level of confidence. If the number of points in the sample N is 
+        less than the default value, k_max will be set automatically to the value ``N/2``.
+    
+    D_thr : float, default=23.92812698
+        Set the level of confidence. The default value corresponds to a p-value of 
+        ``10**{-6}`` for a ``\chiˆ2`` distribution with one degree of freedom.
+
+    dim : int
+        Intrinsic dimensionality of the sample.
+
+    Results
+    -------
+    densities : array [n_samples]
+        The logarithm of the density at each point.
+    
+    err_densities : array [n_samples]
+        The uncertainty in the density estimation, obtained by computing 
+        the inverse of the Fisher information matrix.
+
+    k_hat : array [n_samples]
+        The optimal number of neighbors for which the condition of constant density holds.
+
+    dc : array [n_sample]
+        The radius of the optimal neighborhood for each point.
+    
+    References
+    ----------
+ 
+
+    """
+    # Compute the volume of the dim-sphere with unitary radius
+    V1 = exp(dim/2.*log(pi)-lgamma((dim+2)/2.))    
+    N = distances.shape[0]
+    k_hat = []
+    dc = []
+    densities = []
+    err_densities = []
+    for i in range(0,N):
+        k = 3 # Minimum number of neighbors required
+        Dk = 0 
+        while (k<k_max and Dk<=D_thr):
+            j = k+1
+            # The point i is counted in its neighborhood, so the k-th neighbor is at position k, and not k-1
+            vi = V1*exp(dim*log(distances[i][k])) 
+            vj = V1*exp(dim*log(distances[indices[i][j]][k])) 
+            Dk = -2.*k*(log(vi)+log(vj)-2.*log(vi+vj)+log(4.))
+            k += 1
+        k_hat.append(k-1)
+        dc.append(distances[i][k-1]) #(k-1)-th neighbor is at position k-1
+        densities.append(log(k-1)-(log(V1)+dim*log(dc[i]))) 
+        err_densities.append(sqrt((4.*(k-1)+2.)/((k-1)*(k-2))))
+
+    return densities, err_densities, k_hat, dc
+
+
+class PointAdaptive_kNN(BaseEstimator, DensityMixin):
     """ The pointwise-adaptive k-NN density estimator.
 
     Parameters
@@ -43,14 +113,71 @@ class PointAdaptive_kNN(BaseEstimator):
         equal to n_samples. If dim_algo is a string, it must be one of the options allowed by VALID_DIM. 
 
     dim : int
-        Intrinsic dimensionality of the sample. If dim is provaded, dim_algo is ignored.
+        Intrinsic dimensionality of the sample. If dim is provided, dim_algo is ignored.
+
+    n_jobs : int or None, optional (default=None)
+        The number of jobs to use for the computation. This works by computing
+        each of the n_init runs in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+
+    Attributes
+    ----------
+    distances_ : array [n_samples, k_max]
+        Distances to the k_max neighbors of each points.
+
+    indices_ : array [n_samples, k_max]
+        Indices of the k_max neighbors of each points.
+ 
+    densities_ : array [n_samples]
+        The logarithm of the density at each point.
+    
+    err_densities_ : array [n_samples]
+        The uncertainty in the density estimation, obtained by computing 
+        the inverse of the Fisher information matrix.
+
+    k_hat_ : array [n_samples]
+        The optimal number of neighbors for which the condition of constant density holds.
+
+    dc_ : array [n_sample]
+        The radius of the optimal neighborhood for each point.
+
+
+    Examples
+    --------
+    >>> from DPApipe.DPA import PAk
+    >>> import numpy as np
+    >>> X = np.array([[1, 1], [2, 1], [1, 0],
+    ...               [4, 7], [3, 5], [3, 6]])
+    >>> est = PAk.PointAdaptive_kNN().fit(X)
+    >>> est.distances_
+    array([[0.        , 1.        , 1.        ],
+           [0.        , 1.        , 1.41421356],
+           [0.        , 1.        , 1.41421356],
+           [0.        , 1.41421356, 2.23606798],
+           [0.        , 1.        , 2.23606798],
+           [0.        , 1.        , 1.41421356]])
+    >>> est.indices_
+    array([[0, 1, 2],
+           [1, 0, 2],
+           [2, 0, 1],
+           [3, 5, 4],
+           [4, 5, 3],
+           [5, 4, 3]])    
+    
+    References
+    ----------
+
+        
     """
-    def __init__(self, k_max=1000, D_thr=23.92812698, metric="euclidean", dim_algo="auto", dim=None):
+    def __init__(self, k_max=1000, D_thr=23.92812698, metric="euclidean", dim_algo="auto", dim=None, n_jobs=None):
         self.k_max = k_max
         self.D_thr = D_thr
         self.metric = metric
         self.dim_algo = dim_algo
         self.dim = dim
+        self.n_jobs = n_jobs
 
         if metric not in VALID_METRIC:
             raise ValueError("invalid metric: '{0}'".format(metric))
@@ -83,21 +210,38 @@ class PointAdaptive_kNN(BaseEstimator):
 
         if not self.dim:
             if self.dim_algo == "auto":
-                self.dim = X.shape[0]
+                self.dim = X.shape[1]
             elif self.dim_algo == "two_nn":
-                self.dim = X.shape[0] #two_nn().fit(X)
+                self.dim = X.shape[1] #two_nn().fit(X)
             else:
                 pass
      
         if self.k_max > X.shape[0]:
             self.k_max = int(X.shape[0]/2)
+        if self.k_max < 3:
+            raise ValueError("k_max is below 3, the minimum value required for \
+                        statistical significance. Please use a larger datasets.")
 
         if self.metric == "precomputed":
-            nbrs = NearestNeighbors(n_neighbors=self.k_max, algorithm="auto", metric=self.metric).fit(X)
+            nbrs = NearestNeighbors(n_neighbors=self.k_max+1, # The point i is counted in its neighborhood 
+                                          algorithm="auto", 
+                                        metric=self.metric,
+                                        n_jobs=self.n_jobs).fit(X)
         else:
-            nbrs = NearestNeighbors(n_neighbors=self.k_max, algorithm="brute", metric=self.metric).fit(X)
+            nbrs = NearestNeighbors(n_neighbors=self.k_max+1, # The point i is counted in its neighborhood 
+                                         algorithm="brute", 
+                                        metric=self.metric, 
+                                        n_jobs=self.n_jobs).fit(X)
+            #self.matrix_ = kneighbors_graph(X, n_neighbors=self.k_max,
+            #                                        mode="distance",
+            #                                        include_self=True)
         self.distances_, self.indices_ = nbrs.kneighbors(X) 
 
+        self.densities_, self.err_densities_, self.k_hat_, self.dc_ = _PointAdaptive_kNN(self.distances_, 
+                                                                                 self.indices_,
+                                                                              k_max=self.k_max, 
+                                                                              D_thr=self.D_thr, 
+                                                                                  dim=self.dim)
         self.is_fitted_ = True
         return self
 
--- DPA/tests/test_PAk.py
+++ DPA/tests/test_PAk.py
@@ -1,5 +1,8 @@
 import pytest
 import numpy as np
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+import numpy.testing as npt
 
 from sklearn.datasets import load_iris
 from sklearn.utils.testing import assert_array_equal
@@ -9,21 +12,45 @@ from DPA import PointAdaptive_kNN
 
 
 @pytest.fixture
-def data():
-    return load_iris(return_X_y=True)
+def data_Fig1():
+    data_F1 = pd.read_csv("./benchmarks/Fig1.dat", sep=" ", header=None)
+    return data_F1
+    #return load_iris(return_X_y=True)
 
-def test_PointAdaptive_kNN(data):
-    est = PointAdaptive_kNN()
+@pytest.fixture
+def output_Fig1_test1():
+    out_F1 = pd.read_csv("./benchmarks/output_Fig1_test1.csv", header=None)
+    out_F1.columns = ["rho","rho_err","k_hat","dc"]
+    return out_F1
+
+def test_PointAdaptive_kNN(data_Fig1, output_Fig1_test1):
+    est = PointAdaptive_kNN(n_jobs=-1)
     assert est.dim == None
     assert est.k_max == 1000
     assert est.D_thr == 23.92812698
     assert est.metric == "euclidean"
     assert est.dim_algo == "auto"    
 
-    est.fit(data[0])
+    est.fit(data_Fig1)
     assert hasattr(est, 'is_fitted_')
 
-    print(est.distances_)
+    assert est.k_max == max(est.k_hat_)+1 # k_max include the point i
+    assert len(data_Fig1) == len(est.densities_)
 
+    npt.assert_almost_equal(est.densities_,output_Fig1_test1['rho'], decimal=2)
+    npt.assert_almost_equal(est.err_densities_,output_Fig1_test1['rho_err'], decimal=2)
+    npt.assert_almost_equal(est.dc_,output_Fig1_test1['dc'], decimal=2)
+    #npt.assert_almost_equal(est.k_hat_,output_Fig1_test1['k_hat'], decimal=1)
+    #assert_array_equal(est.densities_,output_Fig1_test1['rho'])
+    df_results = pd.DataFrame(index=range(0,len(est.densities_)), columns=["rho","rho_err","k_hat","dc"])
+    df_results["rho"] = est.densities_
+    df_results["rho_err"] = est.err_densities_
+    df_results["k_hat"] = est.k_hat_
+    df_results["dc"] = est.dc_
+
+    df_bm = output_Fig1_test1
+    print(df_results.head())
+    print(df_bm.head())
+    #assert_frame_equal(df_results,df_bm)
 
 
