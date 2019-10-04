9799585b317fbec43decd1974406db8af03190b2
Maria d'Errico <derrico@immunology.uzh.ch>
2019-10-04 18:49:46 +0200

    Fix package name

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-cd DPA
+echo=$(python setup.py install) 
+cd Pipeline
 echo=$(f2py -c NRmaxL.f90 -m NR)
 echo=$(easycython _DPA.pyx)
 echo=$(easycython _PAk.pyx)
 cd ..
-echo=$(python setup.py install) 
